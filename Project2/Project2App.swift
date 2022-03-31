30cc59ad4c0cf4ed7ea75b2185c58df25bfd9e99
Dan Payne <dpayne@Dans-MacBook-Pro.local>
2022-03-30 19:26:52 -0600

    Initial Commit

---
+++ Project2/Project2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project2App.swift
+//  Project2
+//
+//  Created by Dan Payne on 3/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
